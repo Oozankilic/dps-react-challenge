8d6cc8baeb15e7c5241e24ca6d799a48418d06a1
Ozan <babakin4@gmail.com>
2024-07-14 20:45:47 +0200

    fetch users

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+	const [users, setUsers] = useState([]);
+
+	  useEffect(() => {
+		axios.get('https://dummyjson.com/users')
+		  .then(response => {
+			setUsers(response.data.users);
+		  })
+		  .catch(error => console.error('Error fetching data:', error));
+	  }, []);
+
 	return (
 		<>
 			<div className="home-card">
