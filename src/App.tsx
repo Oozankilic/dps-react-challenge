5bfdb8cc105b7637e259371f4dc39ab9e151c615
Ozan <babakin4@gmail.com>
2024-07-14 22:08:33 +0200

    implement debounced name filter

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,18 @@ function App() {
 
 	const [highlightOldest, setHighlightOldest] = useState(false);
 
-	  useEffect(() => {
-		axios.get('https://dummyjson.com/users')
-		  .then(response => {
-			setUsers(response.data.users);
-			setFilteredUsers(response.data.users);
-		  })
-		  .catch(error => console.error('Error fetching data:', error));
-	  }, []);
+	let debounceTimer;
 
-	  const handleNameFilter = (event) => {
-		const { value } = event.target;
+	useEffect(() => {
+	axios.get('https://dummyjson.com/users')
+		.then(response => {
+		setUsers(response.data.users);
+		setFilteredUsers(response.data.users);
+		})
+		.catch(error => console.error('Error fetching data:', error));
+	}, []);
+
+	const handleNameFilter = (value) => {
 		setNameFilter(value);
 		const lowercasedValue = value.toLowerCase();
 		const filtered = users.filter(user => 
@@ -30,9 +31,9 @@ function App() {
 		  user.lastName.toLowerCase().includes(lowercasedValue)
 		);
 		setFilteredUsers(filtered);
-	  };
+	};
 
-	  const handleCityChange = (event) => {
+	const handleCityChange = (event) => {
 		const { value } = event.target;
 		setCityFilter(value);
 		if(value == ""){
@@ -42,30 +43,36 @@ function App() {
 			const filtered = users.filter(user => user.address.city === value);
 			setFilteredUsers(filtered);
 		}
-	  };
+	};
 
-	  const getOldestUsers = (users) => {
+	const getOldestUsers = (users) => {
 		const oldestPerCity = new Map();
 		users.forEach(user => {
-		  const city = user.address.city;
-		  if (!oldestPerCity.has(city) || new Date(user.birthDate) < new Date(oldestPerCity.get(city).birthDate)) {
+			const city = user.address.city;
+			if (!oldestPerCity.has(city) || new Date(user.birthDate) < new Date(oldestPerCity.get(city).birthDate)) {
 			oldestPerCity.set(city, user);
-		  }
+			}
 		});
 		return oldestPerCity;
-	  };
-	  
-	  const oldestUsers = getOldestUsers(users);
-	  const isOldest = (user) => {
+	};
+	
+	const oldestUsers = getOldestUsers(users);
+	const isOldest = (user) => {
 		return oldestUsers.get(user.address.city).id === user.id;
-	  };
+	};
+
+	const handleDebouncedNameChange = (event) => {
+		clearTimeout(debounceTimer);
+		const value = event.target.value;
+		debounceTimer = setTimeout(() => handleNameFilter(value), 1000);
+	};
 
 	return (
 		<>
 		<input
 			type="text"
 			placeholder="Search by name..."
-			onChange={handleNameFilter}
+			onChange={handleDebouncedNameChange}
 		/>
 		<select onChange={handleCityChange} value={cityFilter}>
 			<option value="">Select city</option>
