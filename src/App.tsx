ec603e382c28293d0c6aba8fcb7b62a5fed4b0ec
Ozan <babakin4@gmail.com>
2024-07-14 20:56:21 +0200

    add name filter

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import axios from 'axios';
 
 function App() {
 	const [users, setUsers] = useState([]);
+	const [filteredUsers, setFilteredUsers] = useState([]);
+	const [nameFilter, setNameFilter] = useState('');
 
 	  useEffect(() => {
 		axios.get('https://dummyjson.com/users')
@@ -13,10 +15,26 @@ function App() {
 		  .catch(error => console.error('Error fetching data:', error));
 	  }, []);
 
+	  const handleNameFilter = (event) => {
+		const { value } = event.target;
+		setNameFilter(value);
+		const lowercasedValue = value.toLowerCase();
+		const filtered = users.filter(user => 
+		  user.firstName.toLowerCase().includes(lowercasedValue) || 
+		  user.lastName.toLowerCase().includes(lowercasedValue)
+		);
+		setFilteredUsers(filtered);
+	  };
+
 	return (
 		<>
+		<input
+			type="text"
+			placeholder="Search by name..."
+			onChange={handleNameFilter}
+		/>
 		<ul>
-		  {users.map(user => (
+		  {filteredUsers.map(user => (
 			<li key={user.id}>
 			  {user.firstName} {user.lastName} - {user.address.city}, {user.birthDate}
 			</li>
